71c0080babeff0d468e112efce2dde5d5864a4e0
Mauri Saavedra <maurisaavedrar@hotmail.com>
2016-05-14 17:24:49 -0300

    New V2.8 fixed
    this version works with heroku

--- packages/telescope-tags/lib/client/templates/autoform_category.js
+++ packages/telescope-tags/lib/client/templates/autoform_category.js
@@ -9,6 +9,8 @@ AutoForm.addInputType("bootstrap-category", {
   }
 });
 
+// -------- Reference Template --------- //
+
 Template.afCategory_bootstrap3.helpers({
   menuItems: function () {
 
@@ -53,6 +55,160 @@ Template.afCategory_bootstrap3.events({
     }
   }
 });
+
+
+// -------- Location Menu --------- //
+
+
+Template.afCategory_location.helpers({
+  menuItems: function () {
+
+    var selectedCategoriesIds = this.value;
+    var prefilledCategoriesIds = _.pluck(Session.get("prefilledCategories"), "_id");
+
+    selectedCategoriesIds = _.compact(prefilledCategoriesIds.concat(selectedCategoriesIds));
+
+    var menuItems = _.map(Categories.find({categoryType: "location"}).fetch(), function (category) {
+
+      var isSelected = _.contains(selectedCategoriesIds, category._id);
+      return {
+        _id: category._id,
+        parentId: category.parentId,
+        template: "category_input_item",
+        label: category.name,
+        isSelected: isSelected,
+        isExpanded: isSelected,
+        itemClass: "category-"+category.slug,
+        data: category
+      };
+    });
+    return menuItems;
+  },
+  atts: function addFormControlAtts() {
+    var atts = _.clone(this.atts);
+    // Add bootstrap class
+    atts = AutoForm.Utility.addClass(atts, "form-control");
+    return atts;
+  }
+});
+
+Template.afCategory_location.onDestroyed(function () {
+  Session.set("prefilledCategories", null);
+});
+
+Template.afCategory_location.events({
+  "click .category-input-item label": function (e) {
+    // only trigger on actual checkbox' click event, and if the checkbox has just been checked
+    if ($(e.toElement).is("input") && $(e.toElement).prop("checked")) {
+      // when marking a category as checked, check all checkboxes of all parent nodes as well
+      $(e.currentTarget).parentsUntil('.category-input', ".menu-item").find(">.menu-item-wrapper input:checkbox").prop("checked", true);
+    }
+  }
+});
+
+
+
+// -------- Topic Menu --------- //
+
+
+
+Template.afCategory_topic.helpers({
+  menuItems: function () {
+
+    var selectedCategoriesIds = this.value;
+    var prefilledCategoriesIds = _.pluck(Session.get("prefilledCategories"), "_id");
+
+    selectedCategoriesIds = _.compact(prefilledCategoriesIds.concat(selectedCategoriesIds));
+
+    var menuItems = _.map(Categories.find({categoryType: "topic"}).fetch(), function (category) {
+
+      var isSelected = _.contains(selectedCategoriesIds, category._id);
+      return {
+        _id: category._id,
+        parentId: category.parentId,
+        template: "category_input_item",
+        label: category.name,
+        isSelected: isSelected,
+        isExpanded: isSelected,
+        itemClass: "category-"+category.slug,
+        data: category
+      };
+    });
+    return menuItems;
+  },
+  atts: function addFormControlAtts() {
+    var atts = _.clone(this.atts);
+    // Add bootstrap class
+    atts = AutoForm.Utility.addClass(atts, "form-control");
+    return atts;
+  }
+});
+
+Template.afCategory_topic.onDestroyed(function () {
+  Session.set("prefilledCategories", null);
+});
+
+Template.afCategory_topic.events({
+  "click .category-input-item label": function (e) {
+    // only trigger on actual checkbox' click event, and if the checkbox has just been checked
+    if ($(e.toElement).is("input") && $(e.toElement).prop("checked")) {
+      // when marking a category as checked, check all checkboxes of all parent nodes as well
+      $(e.currentTarget).parentsUntil('.category-input', ".menu-item").find(">.menu-item-wrapper input:checkbox").prop("checked", true);
+    }
+  }
+});
+
+
+// -------- Source Menu --------- //
+
+
+Template.afCategory_source.helpers({
+  menuItems: function () {
+
+    var selectedCategoriesIds = this.value;
+    var prefilledCategoriesIds = _.pluck(Session.get("prefilledCategories"), "_id");
+
+    selectedCategoriesIds = _.compact(prefilledCategoriesIds.concat(selectedCategoriesIds));
+
+    var menuItems = _.map(Categories.find({categoryType: "source"}).fetch(), function (category) {
+
+      var isSelected = _.contains(selectedCategoriesIds, category._id);
+      return {
+        _id: category._id,
+        parentId: category.parentId,
+        template: "category_input_item",
+        label: category.name,
+        isSelected: isSelected,
+        isExpanded: isSelected,
+        itemClass: "category-"+category.slug,
+        data: category
+      };
+    });
+    return menuItems;
+  },
+  atts: function addFormControlAtts() {
+    var atts = _.clone(this.atts);
+    // Add bootstrap class
+    atts = AutoForm.Utility.addClass(atts, "form-control");
+    return atts;
+  }
+});
+
+Template.afCategory_source.onDestroyed(function () {
+  Session.set("prefilledCategories", null);
+});
+
+Template.afCategory_source.events({
+  "click .category-input-item label": function (e) {
+    // only trigger on actual checkbox' click event, and if the checkbox has just been checked
+    if ($(e.toElement).is("input") && $(e.toElement).prop("checked")) {
+      // when marking a category as checked, check all checkboxes of all parent nodes as well
+      $(e.currentTarget).parentsUntil('.category-input', ".menu-item").find(">.menu-item-wrapper input:checkbox").prop("checked", true);
+    }
+  }
+});
+
+
 
 Template.category_input_item.helpers({
   atts: function () {
