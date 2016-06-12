91620406eb792397bc1dd51d5aa7d8a36219126d
Mauri Saavedra <maurisaavedrar@hotmail.com>
2016-06-12 13:31:53 -0300

    V3.1 Georedirection Working

--- packages/telescope-tags/lib/client/templates/autoform_category.js
+++ packages/telescope-tags/lib/client/templates/autoform_category.js
@@ -57,6 +57,81 @@ Template.afCategory_bootstrap3.events({
 });
 
 
+// -------- Calling Functions Geolocation  --------- //
+
+
+Template.afCategory_suggested_location_geoplugin.helpers({
+  geopluginCountry: function () { 
+    return geoplugin_countryName(); 
+  },
+  geopluginCity: function () { 
+    return geoplugin_city(); 
+  },
+  geopluginCountryCode: function () { 
+    return geoplugin_countryCode(); 
+  },
+  geopluginRegion: function () { 
+    return geoplugin_region(); 
+  },
+  geopluginContinentCode: function () { 
+    return geoplugin_continentCode(); 
+  },
+  cityGeobytes: function () { 
+    return sGeobytesCity; 
+  }
+});
+
+// -------- Suggested Locations Menu Geolocation --------- //
+
+Template.afCategory_suggested_location_geoplugin.helpers({
+  menuItems: function () {
+
+    var selectedCategoriesIds = this.value;
+    var prefilledCategoriesIds = _.pluck(Session.get("prefilledCategories"), "_id");
+
+    selectedCategoriesIds = _.compact(prefilledCategoriesIds.concat(selectedCategoriesIds));
+
+    var menuItems = _.map(Categories.find({name: { $in: [ geoplugin_city(), sGeobytesCity, geoplugin_countryName() ] }}).fetch(), function (category) {
+
+      var isSelected = _.contains(selectedCategoriesIds, category._id);
+      return {
+        _id: category._id,
+        template: "category_input_item",
+        label: category.name,
+        isSelected: isSelected,
+        isExpanded: isSelected,
+        itemClass: "category-"+category.slug,
+        data: category
+      };
+    });
+    return menuItems;
+  },
+  atts: function addFormControlAtts() {
+    var atts = _.clone(this.atts);
+    // Add bootstrap class
+    atts = AutoForm.Utility.addClass(atts, "form-control");
+    return atts;
+  }
+});
+
+
+Template.afCategory_suggested_location_geoplugin.onDestroyed(function () {
+  Session.set("prefilledCategories", null);
+});
+
+Template.afCategory_suggested_location_geoplugin.events({
+  "click .category-input-item label": function (e) {
+    // only trigger on actual checkbox' click event, and if the checkbox has just been checked
+    if ($(e.toElement).is("input") && $(e.toElement).prop("checked")) {
+      // when marking a category as checked, check all checkboxes of all parent nodes as well
+      $(e.currentTarget).parentsUntil('.category-input', ".menu-item").find(">.menu-item-wrapper input:checkbox").prop("checked", true);
+    }
+  }
+});
+
+
+
+
 // -------- Location Menu --------- //
 
 
@@ -217,3 +292,4 @@ Template.category_input_item.helpers({
     }
   }
 });
+
